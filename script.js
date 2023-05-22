77a6d014b692d1abefcb42cbebe9c962dd5ed581
Mohit Kamdi <130160983+Mohit27071998@users.noreply.github.com>
2023-05-22 16:52:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
 //your JS code here. If required.
-let info =document.getElementById("p")
+let info =document.getElementById("browser-info")
 info.innerText = `You are using ${navigator.appName} ${navigator.appVersion}`
